516dfbd3ac8d08204f7f222e59c9adfefb63ad56
forkingD <128193769+forkingD@users.noreply.github.com>
2023-04-21 19:39:54 +1000

    init

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-import gradio
+import gradio as gr
+import openai 
+openai.api_key = "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"
 
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gradio.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+def generate_reply(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+        #stop="\n\n"
+        )
+    message = response['choices'][0]['message']['content']
+    cost = response['usage']['total_tokens']
+    return message.strip()
+
+
+interface = gr.Interface(
+    fn=generate_reply,
+    inputs=gr.inputs.Textbox(label="User Input"),
+    outputs=gr.outputs.Textbox(label="Chatbot Response")
+)
+
+interface.launch()
