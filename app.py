490d48a6126dff6a13c68737402479e6a8e9a997
forkingD <128193769+forkingD@users.noreply.github.com>
2023-04-21 18:49:39 +1000

    init

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ def greet(name):
     return "Hello " + name + "!"
 
 demo = gradio.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch(server_name=0.0.0.0, server_port=8000)
+demo.launch(host="0.0.0.0", port=8000)
